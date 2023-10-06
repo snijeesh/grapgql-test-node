27a3cb26a8ee9681580124d441a5ab8ef35bf76e
Snijeesh Palott <spalott@spalott46641.home>
2023-10-06 15:58:00 -0700

    Made changes to start apollo server.

---
+++ index.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const { url } = await startStandaloneServer(server)
+
+console.log(`🚀 Server ready at ${url}`)
